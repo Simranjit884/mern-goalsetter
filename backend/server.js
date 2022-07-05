a3a1794b4ed595ebe4d7b24d178cbc3b7008ee75
samy884 <ssgill884@gmail.com>
2022-07-05 18:53:51 +0530

    calling connectDB

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const {errorHandler} = require('./middleware/errorMiddleWare');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
