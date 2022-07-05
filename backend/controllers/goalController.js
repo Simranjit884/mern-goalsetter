b19b1af73e9d49b3d37323c6bc38a1a376b2ec4a
samy884 <ssgill884@gmail.com>
2022-07-05 19:34:31 +0530

    put request for updatedgoal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -15,14 +15,23 @@ const setGoals = asyncHandler(async (req, res) => {
   }
 
   const goal = await Goal.create({
-    text: req.body.text
+    text: req.body.text,
   });
 
   res.status(200).json(goal);
 });
 
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
 
 const deleteGoal = asyncHandler(async (req, res) => {
